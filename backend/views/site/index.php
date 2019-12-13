5416ea8f800ea9c57beab3bde7b52603ffac55ea
Manop Kongoon <kongoon@hotmail.com>
2019-12-13 14:38:44 +0700

    update

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -10,7 +10,11 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="card">
     <div class="card-body">
         <?= Highcharts::widget([
+            
             'options' => [
+                /*'chart' => [
+                    'type' => 'column'
+                ],*/
                 'title' => ['text' => 'จำนวนการแจ้งซ่อมปี '.date('Y')],
                 'xAxis' => [
                     'categories' => ['ม.ค.', 'ก.พ.', 'มี.ค.', 'เม.ย', 'พ.ค.', 'มิ.ย.', 'ก.ค.', 'ส.ค.', 'ก.ย.', 'ต.ค.', 'พ.ย.', 'ธ.ค.']
@@ -26,4 +30,4 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 </div>
 
-<?php var_dump($data)?>
+<?php //var_dump($data)?>
