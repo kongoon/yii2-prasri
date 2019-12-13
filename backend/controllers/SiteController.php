915105fd2adb199d70dae1ed6056076b51d36fee
Manop Kongoon <kongoon@hotmail.com>
2019-12-13 14:32:55 +0700

    update

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -60,7 +60,43 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $sql = "
+        SELECT
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '01' THEN  1 ELSE 0 END) AS m01,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '02' THEN  1 ELSE 0 END) AS m02,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '03' THEN  1 ELSE 0 END) AS m03,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '04' THEN  1 ELSE 0 END) AS m04,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '05' THEN  1 ELSE 0 END) AS m05,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '06' THEN  1 ELSE 0 END) AS m06,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '07' THEN  1 ELSE 0 END) AS m07,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '08' THEN  1 ELSE 0 END) AS m08,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '09' THEN  1 ELSE 0 END) AS m09,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '10' THEN  1 ELSE 0 END) AS m10,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '11' THEN  1 ELSE 0 END) AS m11,
+        SUM(CASE WHEN DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%m') = '12' THEN  1 ELSE 0 END) AS m12
+
+        FROM fix_transaction
+        WHERE DATE_FORMAT(FROM_UNIXTIME(transaction_at), '%Y') = '".date('Y')."'
+        ";
+
+        $data = Yii::$app->db->createCommand($sql)->queryAll();
+        $data = [
+            (int) $data[0]['m01'],
+            (int) $data[0]['m02'],
+            (int) $data[0]['m03'],
+            (int) $data[0]['m04'],
+            (int) $data[0]['m05'],
+            (int)  $data[0]['m06'],
+            (int) $data[0]['m07'],
+            (int) $data[0]['m08'],
+            (int) $data[0]['m09'],
+            (int) $data[0]['m10'],
+            (int) $data[0]['m11'],
+            (int) $data[0]['m12']
+        ];
+        return $this->render('index', [
+            'data' => $data
+        ]);
     }
 
     /**
